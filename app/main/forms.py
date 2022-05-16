43e861ccfaa0700d861776ad2212a647c02c08db
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-16 20:19:15 +0300

    added more classes

---
+++ app/main/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,ValidationError
+from wtforms.validators import DataRequired
+from ..models import Subscribe
+class CommentForm(FlaskForm):
+     comment = TextAreaField('Comment',validators = [DataRequired()])
+     submit = SubmitField('Submit')
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us more about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+class BlogForm(FlaskForm):
+     title = StringField('Title',validators = [DataRequired()])
+     blog_text = TextAreaField('Write your blog...',validators = [DataRequired()])
+     submit = SubmitField('Submit')
+     
+class UpdateBlog(FlaskForm):
+    text = TextAreaField('Edit blog ',validators = [DataRequired()])
+    submit = SubmitField('Update')
+
+class UpdateComment(FlaskForm):
+    text = TextAreaField('Edit comment ',validators = [DataRequired()])
+    submit = SubmitField('Update')
+class SubscribeForm(FlaskForm):
+    email = StringField('Enter your  email',validators = [DataRequired()])
+    username = StringField('Username', validators = [DataRequired()])
+    submit = SubmitField('Subscribe')
+
+    def validate_email(self,data_field):
+        if Subscribe.query.filter_by(email = data_field.data).first():
+            raise ValidationError('This information exists.') 
